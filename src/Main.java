29376b4a956f1c6d34b619cb9b123daa073616f4
EanPhillips8 <eanphillips8@gmail.com>
2022-09-18 12:50:45 -0400

    Git

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double creditBal = 5000;
+        double interest = 0.17;
+        double total1 = creditBal * interest;
+        double month1 = total1 + creditBal;
+        double total2 = month1 * interest;
+        double month2 = (month1 + total2);
+
+        System.out.println("Your total plus interest for this month is " + month1);
+
+        System.out.println("Your total plus interest for next month is " + month2);
+
+
+
+
+
     }
 }
